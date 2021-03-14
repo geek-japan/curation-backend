ab9e61869d712207b360f240337550bfebedb309
gpioblink <gpioblink@gmail.com>
2021-03-15 00:34:55 +0900

    add fechArticleDetail

--- cloud/functions/localTestServer.py
+++ cloud/functions/localTestServer.py
@@ -2,12 +2,17 @@ from flask import Flask, request
 from functions.searchArticles import search_articles
 from functions.cors import run_function_with_cors_enabled
 from functions.demo_fetchArticleDetail import demo_fetchArticleDetail
+from functions.fetchArticleDetail import fetchArticleDetail
 
 app = Flask(__name__)
 
 @app.route('/search', methods=['GET'])
 def search():
     return run_function_with_cors_enabled(request, search_articles)
+
+@app.route('/detail', methods=['GET', 'POST'])
+def detail():
+    return run_function_with_cors_enabled(request, fetchArticleDetail)
 
 @app.route('/demo_detail', methods=['GET', 'POST'])
 def demo_detail():
