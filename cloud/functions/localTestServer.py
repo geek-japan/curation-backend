7df0430964d58d5bb2b1363581a93e20f7979743
gpioblink <gpioblink@gmail.com>
2021-03-14 16:08:53 +0900

    add demo article detail

--- cloud/functions/localTestServer.py
+++ cloud/functions/localTestServer.py
@@ -1,11 +1,17 @@
 from flask import Flask, request
 from functions.searchArticles import search_articles
+from functions.cors import run_function_with_cors_enabled
+from functions.demo_fetchArticleDetail import demo_fetchArticleDetail
 
 app = Flask(__name__)
 
 @app.route('/search', methods=['GET'])
 def search():
-    return search_articles(request)
+    return run_function_with_cors_enabled(request, search_articles)
+
+@app.route('/demo_detail', methods=['GET', 'POST'])
+def demo_detail():
+    return run_function_with_cors_enabled(request, demo_fetchArticleDetail)
 
 
 if __name__ == '__main__':
